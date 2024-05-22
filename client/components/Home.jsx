75c9c0db9b28ec2f39dd53ca83e239ad57683d40
sweetchin23 <nacelle.anne@gmail.com>
2024-05-22 12:02:43 -0400

    first commit

---
+++ client/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+     return (
+          <div>
+               <h1> Welcome to My Portfolio</h1>
+               <p>add link/butoon to about me page and a mission statement</p>
+          </div>
+     )
+}
+<link to = "/about"></link>
